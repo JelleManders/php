12d1b7b186f8319ed2640059edd5921ff148e2f5
Roemer Bakker <contact@roemerbakker.com>
2017-02-15 14:47:35 +0100

    Integrated Klarna, alongside PClass, Good and GoodsList object classes. Changed Guzzle payload to JSON instead of form-params

---
+++ src/Complexity/SpryngPaymentsApiPhp/Object/GoodsList.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SpryngPaymentsApiPhp\Object;
+
+use SpryngPaymentsApiPhp\Object\Good;
+
+/**
+ * Class GoodsList
+ * @package SpryngPaymentsApiPhp\Object
+ */
+class GoodsList
+{
+    /**
+     * Array which holds the Good objects.
+     *
+     * @var array
+     */
+    protected $goods = array();
+
+    /**
+     * Adds a new good to the list.
+     *
+     * @param Good $good
+     */
+    public function add(Good $good)
+    {
+        array_push($this->goods, $good);
+    }
+
+    /**
+     * Removes a good from the list.
+     *
+     * @param $key
+     */
+    public function remove($key)
+    {
+        unset($this->goods[$key]);
+    }
+
+    /**
+     * Turns the goods list into a JSON string.
+     *
+     * @return string
+     */
+    public function toJson()
+    {
+        return json_encode($this->goods);
+    }
+
+    public function toArray()
+    {
+        $goods = array();
+
+        foreach($this->goods as $good)
+        {
+            array_push($goods, get_object_vars($good));
+        }
+
+        return $goods;
+    }
+}
